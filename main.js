fc5aca2f8546c4cfa47d90886702110301c55673
JongInSon <songane@gmail.com>
2019-04-18 15:42:27 +0900

    0418

--- main.js
+++ main.js
@@ -1,73 +1,67 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathName = url.parse(_url, true).pathname;
 
-    if (pathName === '/') {      
-      if (queryData.id === undefined) {
-        
-        fs.readdir('./data', function(err, fileList) {
-          console.log(fileList);
-        })
-        
+function templateHTML(title, list, body) {
+  return `<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+</body>
+</html>
+`;
+}
+
+function templateList(fileList) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < fileList.length) {
+    list = list + `<li><a
+    href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathName = url.parse(_url, true).pathname;
+
+  if (pathName === '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (err, fileList) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
-          var template = 
-          `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+        var list = templateList(fileList);
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
 
-      } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        response.writeHead(200);
+        response.end(template);
+      })
+
+    } else {
+      fs.readdir('./data', function (err, fileList) {
+        var list = templateList(fileList);
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
           var title = queryData.id;
-          var template = 
-          `<!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
         });
-      }
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
+      });
     }
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
 
 });
-app.listen(3000);
+app.listen(3000)
